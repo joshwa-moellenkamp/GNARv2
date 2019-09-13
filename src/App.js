34db0fdea7438fd99f2c19d9ac833f4ac96522bf
Joshwa Moellenkamp <joshwa.moellenkamp@gmail.com>
2019-09-13 13:46:26 -0600

    Removed unnecessary 'lines' object from initial state; improved conditional rendering based on 'challenges' json data.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { }
+  }
+
+  async componentDidMount() {
+    if(localStorage.getItem('challenges') === null) {
+      await fetch('https://joshwa-moellenkamp.github.io/GNAR/source.json')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        localStorage.setItem('challenges', JSON.stringify(json));
+      })
+      .catch(function() {
+        // TODO
+      });
+    }
+    this.setState({ challenges: JSON.parse(localStorage.getItem('challenges')) })
+  }
+
+  render () {
+    if(this.state.challenges != null && this.state.challenges !== 'undefined') {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>H1</h1>
+            <table>
+              <tbody>
+                { this.renderElements() }
+              </tbody>
+            </table>
+          </header>
+        </div>
+      )
+    } else {
+      return null
+    }
+  }
+
+  renderElements() {
+    var categories = Object.keys(this.state.challenges)
+    console.log(categories)
+    console.log(this.state.challenges[categories[0]])
+    return this.state.challenges[categories[0]].map((challenge, index) => {
+      const { name, points, description } = challenge
+      console.log('name', name)
+      return (
+        <tr key={index}>
+          <td>{name}</td>
+          <td>{points}</td>
+          <td>{description}</td>
+          <td></td>
+        </tr>
+      )
+    })
+  }
 }
 
 export default App;
