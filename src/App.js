880004d2ebfde6f21481a1ed0e35fcdc1c4ebeff
Joshwa Moellenkamp <joshwa.moellenkamp@gmail.com>
2019-09-13 15:02:09 -0600

    Use material ui for table.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import 'typeface-roboto';
 
 class App extends React.Component {
   constructor(props) {
@@ -34,8 +42,7 @@ class App extends React.Component {
               challengeCategories.map((category, index) => {
                 return (
                   <div>
-                    <h3>{category}</h3>
-                    { this.renderElements(challenges[challengeCategories[index]]) }
+                    { this.renderElements(challengeCategories[index], challenges[challengeCategories[index]]) }
                   </div>
                 )
               })
@@ -48,22 +55,32 @@ class App extends React.Component {
     }
   }
 
-  renderElements(challenges) {
-    return challenges.map((challenge, index) => {
-      const { name, points, description } = challenge
-      return (
-        <table>
-          <tbody>
-            <tr key={index}>
-              <td>{name}</td>
-              <td>{points}</td>
-              <td>{description}</td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-      )
-    })
+  renderElements(category, challenges) {
+    return (
+      <Card>
+        <Typography variant="h5" component="h2">
+          category
+        </Typography>
+        <Table style={{ tableLayout: "auto" }}>
+          <TableHead>
+            <TableRow>
+              <TableCell style={{ width: "30%" }}>Challenge</TableCell>
+              <TableCell style={{ width: "10%" }}>Point Value</TableCell>
+              <TableCell style={{ width: "60%" }}>Description</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {challenges.map((challenge, index) => (
+              <TableRow key={index}>
+                <TableCell>{challenge.name}</TableCell>
+                <TableCell>{challenge.points}</TableCell>
+                <TableCell>{challenge.description}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Card>
+    )
   }
 }
 
